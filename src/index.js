93d5acd80882d1c91408c6bee5a55354cb05ff56
sagarv8090 <sagarvishwakarma08032000@gmai.com>
2025-04-06 00:59:44 +0530

    connection to MongoDB Atlas

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import express from 'express';
+import connectDB from './db/index.js';
+const app = express();
+
+dotenv.config({
+    path:'.env'
+});
+
+connectDB();
